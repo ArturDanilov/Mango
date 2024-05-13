732560686e461fdd303e0e36ffa1490a650fe5ab
Artur Danilov <arthurdanilov1990@gmail.com>
2024-05-13 17:48:15 +0200

    Add CouponDto and DbContext

--- Mango.Services.CouponAPI/Controllers/Models/Coupon.cs
+++ Mango.Services.CouponAPI/Controllers/Models/Coupon.cs
@@ -1,11 +1,20 @@
-﻿namespace Mango.Services.CouponAPI.Controllers.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Services.CouponAPI.Controllers.Models
 {
     public class Coupon
     {
+        [Key]
         public int CouponId { get; set; }
+
+        [Required]
         public string CouponName { get; set; } = String.Empty;
+
+        [Required]
         public string DiscountAmout{ get; set; } = String.Empty;
+        
         public int MinAmout { get; set; }
+        
         public DateTime LastUpdate { get; set; }
     }
 }
--- Mango.Services.CouponAPI/Controllers/Models/Dto/CouponDto.cs
+++ Mango.Services.CouponAPI/Controllers/Models/Dto/CouponDto.cs
@@ -3,9 +3,13 @@
     public class CouponDto
     {
         public int CouponId { get; set; }
+        
         public string CouponName { get; set; } = String.Empty;
+        
         public string DiscountAmout { get; set; } = String.Empty;
+        
         public int MinAmout { get; set; }
+        
         public DateTime LastUpdate { get; set; }
     }
 }
