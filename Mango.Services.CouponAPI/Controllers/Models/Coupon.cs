69023e8885f89ca1d4b2a168544a9e1adeec6707
Artur Danilov <arthurdanilov1990@gmail.com>
2024-05-13 17:29:18 +0200

    Add project files.

---
+++ Mango.Services.CouponAPI/Controllers/Models/Coupon.cs
@@ -0,0 +1,11 @@
+﻿namespace Mango.Services.CouponAPI.Controllers.Models
+{
+    public class Coupon
+    {
+        public int CouponId { get; set; }
+        public string CouponName { get; set; } = String.Empty;
+        public string DiscountAmout{ get; set; } = String.Empty;
+        public int MinAmout { get; set; }
+        public DateTime LastUpdate { get; set; }
+    }
+}
---
+++ Mango.Services.CouponAPI/Controllers/Models/Dto/CouponDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Mango.Services.CouponAPI.Controllers.Models.Dto
+{
+    public class CouponDto
+    {
+        public int CouponId { get; set; }
+        public string CouponName { get; set; } = String.Empty;
+        public string DiscountAmout { get; set; } = String.Empty;
+        public int MinAmout { get; set; }
+        public DateTime LastUpdate { get; set; }
+    }
+}
