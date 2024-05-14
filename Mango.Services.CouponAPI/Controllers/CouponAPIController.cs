03f7a112be81e207bef4c3520641bc0c96ccd1ae
Artur Danilov <arthurdanilov1990@gmail.com>
2024-05-14 17:39:36 +0200

    Add Update and Delete

--- Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -3,6 +3,7 @@ using Mango.Services.CouponAPI.Data;
 using Mango.Services.CouponAPI.Models;
 using Mango.Services.CouponAPI.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
+using NHibernate.Linq;
 
 namespace Mango.Services.CouponAPI.Controllers
 {
@@ -84,6 +85,45 @@ namespace Mango.Services.CouponAPI.Controllers
 
                 _db.SaveChanges();
                 _response.Result = _mapper.Map<CouponDto>(obj);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpPut]
+        public ResponseDto Put([FromBody] CouponDto couponDto)
+        {
+            try
+            {
+                var obj = _mapper.Map<Coupon>(couponDto);
+                _db.Coupons.Update(obj);
+
+                _db.SaveChanges();
+                _response.Result = _mapper.Map<CouponDto>(obj);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public ResponseDto Delete(int id)
+        {
+            try
+            {
+                var coupon = _db.Coupons.First(u => u.CouponId == id);
+                _db.Coupons.Remove(coupon);
+                _db.SaveChanges();
             }
             catch (Exception ex)
             {
