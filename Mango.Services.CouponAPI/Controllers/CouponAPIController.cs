55f520fed9eea3cbed42dd3c07f1a908fe4e1343
Artur Danilov <arthurdanilov1990@gmail.com>
2024-05-15 15:54:06 +0200

    Add Bootstrap; Add CouponController

--- Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -7,7 +7,7 @@ using NHibernate.Linq;
 
 namespace Mango.Services.CouponAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/coupon")]
     [ApiController]
     public class CouponAPIController : ControllerBase
     {
