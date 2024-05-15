3671c52d5167b7a239af95c539a95e033a1074fb
Artur Danilov <arthurdanilov1990@gmail.com>
2024-05-15 15:06:51 +0200

    Implement CRAD Operations for CouponService

---
+++ Mango.Web/Models/CouponDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Mango.Web.Models
+{
+    public class CouponDto
+    {
+        public int CouponId { get; set; }
+
+        public string CouponCode { get; set; } = string.Empty;
+
+        public int DiscountAmout { get; set; }
+
+        public int MinAmout { get; set; }
+    }
+}
