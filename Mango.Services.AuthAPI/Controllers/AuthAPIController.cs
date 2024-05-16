04a333426ab2cc66da5f3cffb26b69b0d8044997
Artur Danilov <arthurdanilov1990@gmail.com>
2024-05-16 14:59:05 +0200

    Login User

--- Mango.Services.AuthAPI/Controllers/AuthAPIController.cs
+++ Mango.Services.AuthAPI/Controllers/AuthAPIController.cs
@@ -31,9 +31,17 @@ namespace Mango.Services.AuthAPI.Controllers
         }
 
         [HttpPost("login")]
-        public async Task<IActionResult> Login()
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto dto)
         {
-            return Ok();
+            var loginResponse = await _authService.Login(dto);
+            if(loginResponse.User == null)
+            {
+                _response.IsSuccess = false;
+                _response.Message = "Username or Password is incorrect";
+                return BadRequest(_response);
+            }
+            _response.Result = loginResponse;
+            return Ok(loginResponse);
         }
     }
 }
