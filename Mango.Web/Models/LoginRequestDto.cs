2a416625f2b7202ed8c79b066f110b40e0dec35d
Artur Danilov <arthurdanilov1990@gmail.com>
2024-05-17 15:32:05 +0200

    Validation with Login and Register

--- Mango.Web/Models/LoginRequestDto.cs
+++ Mango.Web/Models/LoginRequestDto.cs
@@ -1,8 +1,13 @@
-﻿namespace Mango.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Web.Models
 {
     public class LoginRequestDto
     {
+        [Required]
         public string UserName { get; set; } = string.Empty;
+
+        [Required]
         public string Password { get; set; } = string.Empty;
     }
 }
--- Mango.Web/Models/RegistrationRequestDto.cs
+++ Mango.Web/Models/RegistrationRequestDto.cs
@@ -1,14 +1,19 @@
-﻿namespace Mango.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Web.Models
 {
     public class RegistrationRequestDto
     {
-
+        [Required]
         public string Name { get; set; } = string.Empty;
 
+        [Required]
         public string Email { get; set; } = string.Empty;
 
+        [Required]
         public string PhoneNumber { get; set; } = string.Empty;
 
+        [Required]
         public string Password { get; set; } = string.Empty;
 
         public string? Role { get; set; }
