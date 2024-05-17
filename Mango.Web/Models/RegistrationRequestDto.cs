5998fd48dd24efcf09d90460bf935ed1d56601d0
Artur Danilov <arthurdanilov1990@gmail.com>
2024-05-17 10:00:30 +0200

    Add API URLin Web Projekt

---
+++ Mango.Web/Models/RegistrationRequestDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Mango.Web.Models
+{
+    public class RegistrationRequestDto
+    {
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+
+        public string? Role { get; set; }
+    }
+}
---
+++ Mango.Web/Models/UserDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Mango.Web.Models
+{
+    public class UserDto
+    {
+        public string ID{ get; set; } = string.Empty;
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string PhoneNumber { get; set; } = string.Empty;
+    }
+}
