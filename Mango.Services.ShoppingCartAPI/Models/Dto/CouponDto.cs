f16a4b7319a928f84d75f9fe8ae171da23d7856e
Artur Danilov <arthurdanilov1990@gmail.com>
2024-06-03 14:35:55 +0200

    add ShoppingCartAPI and Dtos

---
+++ Mango.Services.ShoppingCartAPI/Models/Dto/CouponDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Mango.Services.ShoppingCartAPI.Models.Dto
+{
+    public class CouponDto
+    {
+        public int CouponId { get; set; }
+
+        public string CouponCode { get; set; } = string.Empty;
+
+        public int DiscountAmount { get; set; }
+
+        public int MinAmount { get; set; }
+    }
+}
